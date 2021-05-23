52d3c066a7fd8ff7e1fffbb7886e4b406c5a68d8
ayrtonteshima <ayrtonteshima@gmail.com>
2021-05-23 11:06:57 -0300

    feat: servindo js e css hard-coded

--- server.js
+++ server.js
@@ -1,26 +1,49 @@
 import http from 'http';
 
 const server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-type': 'text/html',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*' 
-  });
+  console.log(req.url);
 
-  res.write(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>JS Raiz</title>
-    </head>
-    <body>
-      <h1>Olá raizeros!!!</h1>
-    </body>
-    </html>
-  `);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*'); 
+
+  if (req.url === '/main.js') {
+    res.writeHead(200, {
+      'Content-type': 'text/javascript'
+    });
+    res.write(`
+      function soma(a, b) {
+        return a + b;
+      }
+      console.log('Oi galera!', soma(10, 5));
+    `);
+  } else if (req.url === '/style.css') {
+    res.writeHead(200, {
+      'Content-type': 'text/css',
+    });
+    res.write(`
+      h1 { color: red; }
+    `);
+  } else {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    })
+    res.write(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>JS Raiz</title>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <h1>Olá raizeros!!!</h1>
+        <script src="main.js"></script>
+      </body>
+      </html>
+    `);
+  }
   res.end();
 });
 
